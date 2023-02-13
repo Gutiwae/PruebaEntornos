8dd8ffb3e02a1bea9bb4914b65938fd1b56711e2
Gutiwae <antonioluisgutierrezgodoy@gmail.com>
2023-02-13 14:27:50 +0100

    Calculadora añadida

--- src/App/Main.java
+++ src/App/Main.java
@@ -1,48 +1,16 @@
 package App;
 
+import java.util.stream.DoubleStream;
+
 public class Main {
-    /**
-     *Metodo constructor parametrizado
-     * @param num1 primer numero introducido
-     * @param num2 segundo numero introducido
-     */
-    public int suma(int num1, int num2) {
-        /**
-         * Metodo para regresar el resultado de la suma
-         * @return Regresa la suma entre los parametros num1 y num2
-         */
-        return num1 + num2;
-    }
 
-    public int resta(int num1, int num2) {
-        /**
-         * Metodo para regresar el resultado de la resta
-         * @return Regresa la resta entre los parametros num1 y num2
-         */
-        return num1 - num2;
-    }
+static double add(double... operands) {
+        return DoubleStream.of(operands)
+        .sum();
+        }
 
-    public int multiplica(int num1, int num2) {
-        /**
-         * Metodo para regresar el resultado de la multiplicacion
-         * @return Regresa la multiplicacion entre los parametros num1 y num2
-         */
-        return num1 * num2;
-    }
-
-    public int divide(int num1, int num2) {
-        /**
-         * Metodo que establece un error si se intenta dividir por cero
-         */
-        if (num2 == 0) throw new ArithmeticException("NO PUEDES DIVIDIR POR CERO, MELON");
-        /**
-         * Metodo para regresar el resultado de la division
-         * @return Regresa la division entre los parametros num1 y num2
-         */
-        return num1 / num2;
-    }
-
-    private int valor1;
-    private int valor2;
-}
-
+static double multiply(double... operands) {
+        return DoubleStream.of(operands)
+        .reduce(1, (a, b) -> a * b);
+        }
+        }
