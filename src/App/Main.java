3b74f458d71af065c7dcce1f4d07de89182eeaa8
Gutiwae <antonioluisgutierrezgodoy@gmail.com>
2023-02-14 10:53:44 +0100

    Se han añadido dos cometarios con las funciones de los dos metodos de la calculadora

--- src/App/Main.java
+++ src/App/Main.java
@@ -3,12 +3,12 @@ package App;
 import java.util.stream.DoubleStream;
 @Author Guti
 public class Main {
-
+//Metodo para hacer una suma entre dos variables
 static double add(double... operands) {
         return DoubleStream.of(operands)
         .sum();
         }
-
+//Metodo para multiplicar dos numeros
 static double multiply(double... operands) {
         return DoubleStream.of(operands)
         .reduce(1, (a, b) -> a * b);
